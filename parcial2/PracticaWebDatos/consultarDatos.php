fe0512fa455df994b64216ce158676be196c8d5c
Your Name <you@example.com>
2022-10-27 19:31:49 -0600

    biusqueda de datos

--- parcial2/PracticaWebDatos/consultarDatos.php
+++ parcial2/PracticaWebDatos/consultarDatos.php
@@ -17,38 +17,8 @@
 
     ?>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.html">Práctica Web</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="registrarDatos.html">Registar</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                Opciones
-              </a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Inicio</a>
-                <a class="dropdown-item" href="registarDatos.html">Registrar</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Consultar</a>
-              </div>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-          </form>
-        </div>
-    </nav><br>
+    <?php include 'menu.php'; ?>
+    <br>
     <div class="container">
         <div class="row">
             <div class="col-12">
